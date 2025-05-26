3c053fae8af6dfc0256742a8d2d2f7f7fe2fac68
Balan Razvan <razvan10balan10@gmail.com>
2025-05-26 10:36:42 +0300

    Update firebaseConfig.js

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHltCB_-pqdH4FYgU5OZlaTtw-27P4IW0",
-    authDomain: "sistemmedical-721a1.firebaseapp.com",
-    projectId: "sistemmedical-721a1",
-    storageBucket: "sistemmedical-721a1.firebasestorage.app",
-    messagingSenderId: "384163687233",
-    appId: "1:384163687233:web:7d669bc4bd994b5d98cfc0"
+
   };
 
 const app = initializeApp(firebaseConfig);
